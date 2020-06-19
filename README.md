49dcb56c7c2b36e06a1960a0b5545cd5b2f47f7e
Sarah Holder <sr.holder@yahoo.com>
2020-06-18 20:44:07 -0500

    finished file setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # OG Emoji
+#OG Emoji
---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>OG Emoji</h2>
+        <button className="btn btn-info"><i className="fas fa-rocket"></i></button>
+      </div>
+    );
+  }
+}
+
+export default App;
